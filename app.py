5a9e4fa7cf57ecfdccdb7be2a03ce28ea1816fac
MSI\super <superda1997@gmail.com>
2026-03-06 19:20:05 +0700

    First commit

---
+++ app.py
@@ -0,0 +1,15 @@
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello from CI/CD Demo!"
+
+@app.route('/health')
+def health():
+    return {"status": "healthy"}, 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
